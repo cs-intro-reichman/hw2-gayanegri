3556c8128219523d9346f6ef99e9c9b823fc8118
Gaya Negri <gayanegri@Gayas-MacBook-Air.local>
2025-11-08 21:50:38 +0200

    Completed assignment

---
+++ Collatz.java
@@ -0,0 +1,38 @@
+// Demonstrates the Collatz conjecture.
+public class Collatz {
+	public static void main(String args[]) {
+	    int n = Integer.parseInt(args[0]);
+		char mode = args[1].charAt(0);
+		if (mode == 'v') {
+			System.out.println("1 4 2 1 (4)");
+		}
+		int seed = 2;
+		while (seed <= n) {
+			String row = ("" + seed);
+			int count = 1;
+			int y = seed;
+			while (y > 1) {
+				if (y % 2 == 0) {
+					y = (y / 2);
+				} else {
+					y = (y * 3 + 1);
+				}
+				row = (row + " " + y);
+				count ++ ;
+			}
+			if (mode == 'v' && y == 1 ) {
+				System.out.println(row + " (" + count + ")");
+			}
+			seed++ ;
+		}
+		if (seed == n + 1 && mode == 'v'){
+		    System.out.println();
+		    System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+		} else {
+			if (seed == n + 1 && mode == 'c') {
+				System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+			}
+
+		} // Replace this comment with your code
+	}
+}
