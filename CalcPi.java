a0098bab78646b3183a8fdba1b23e055bb7922da
Gaya Negri <gayanegri@Gayas-MacBook-Air.local>
2025-11-08 23:25:07 +0200

    uploaded my final homework files

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,27 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]);
+		int negative = (n / 2);
+		int x = 3;
+		double fneg = (-1.0 / x);
+		for (int m = 1; m < negative; m++) {
+		    x = (x + 4);
+			fneg = fneg - (1.0 / x);	
+		}
+
+		int positive = ((n + 1) / 2);
+		int y = 1;
+		double fpos = (1.0 / y);
+		for (int m = 1; m < positive; m++) {
+		    y = (y + 4);
+			fpos = fpos + (1.0 / y);	
+		}
+		double piApproximated = ((fpos + fneg) * 4);
+
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + piApproximated);
+			 // Replace this comment with your code
 	}
+
 }
--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,21 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String name = args[0].toUpperCase();
+            int number = Integer.parseInt(args[1]);
+            for (int c = 0; c < name.length(); c++) {
+                char letter = name.charAt(c);
+                String anLetters = "AEFHILMNORSX";
+                if (anLetters.indexOf(letter) == -1) {
+                        System.out.println("Give me a  " + letter + ": " + letter + "!");
+                } else {
+                        System.out.println("Give me an " + letter + ": " + letter + "!");
+                }
+                }
+                System.out.println("What does that spell?");
+                for (int n = 0; n < number;n++) {
+                        System.out.println(name + " ! ! !");
+                }
+            } // Replace this comment with your code
         }
-}
+
