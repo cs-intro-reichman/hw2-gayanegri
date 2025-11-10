4bffa721d3c9ebe7170d8c6a03e053e6147ae971
Gaya Negri <gayanegri@Gayas-MacBook-Air.local>
2025-11-10 08:08:32 +0200

    fixed all

--- CalcPi.java
+++ CalcPi.java
@@ -2,22 +2,18 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    int n = Integer.parseInt(args[0]);
-		int negative = (n / 2);
-		int x = 3;
-		double fneg = (-1.0 / x);
-		for (int m = 1; m < negative; m++) {
-		    x = (x + 4);
-			fneg = fneg - (1.0 / x);	
-		}
-
-		int positive = ((n + 1) / 2);
-		int y = 1;
-		double fpos = (1.0 / y);
-		for (int m = 1; m < positive; m++) {
-		    y = (y + 4);
-			fpos = fpos + (1.0 / y);	
-		}
-		double piApproximated = ((fpos + fneg) * 4);
+		double down = 1.0;
+		double sum = 0.0;
+		for (int i = 0; i < n; i++) {
+			if (i % 2 == 0) {
+				sum = sum + (1 / down);
+			} else {
+				sum = sum - (1 / down);
+			} 
+			down = down + 2;
+		} 	
+		
+		double piApproximated = (sum * 4);
 
 		System.out.println("pi according to Java: " + Math.PI);
 		System.out.println("pi, approximated:     " + piApproximated);
